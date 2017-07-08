7b2cf46804b37a61613e4482f3469ae58e1adf2b
Kari Lou Tant <ktant@att.net>
2017-07-08 00:54:23 -0400

    Update Media.py

--- MovieWebPage/Media.py
+++ MovieWebPage/Media.py
@@ -1,13 +1,15 @@
 import webbrowser
 
+
 class Movie:
-    """this class stores movie information"""
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    """Attributes:
+        movie_title: The movie's title.
+        poster_image_url: The URL for the movie's poster art.
+        trailer_youtube_url: The URL for the movie's Trailer
+    """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
